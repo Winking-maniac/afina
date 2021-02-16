84f15c31101869840cfb645a5abece69243441d4
Winking-maniac <gkgim@yadexru>
2021-02-16 20:25:00 +0300

    Backup

--- .ccls-cache/@home@winking-maniac@github@afina/src@storage@SimpleLRU.h
+++ .ccls-cache/@home@winking-maniac@github@afina/src@storage@SimpleLRU.h
@@ -44,20 +44,31 @@ private:
     using lru_node = struct lru_node {
         std::string key;
         std::string value;
-        std::unique_ptr<lru_node> prev;
+        lru_node *prev;
         std::unique_ptr<lru_node> next;
     };
+
+    // Make a node, put it in list and map without validation check
+    bool _Put(const std::string &key, const std::string &value);
+
+    // Frees *size* bytes in storage. Do nothing when size <= 0
+    bool _FreeSpace(int size);
+
+    // Moves *node* to the head of list
+    void _MakeFirst(std::reference_wrapper<lru_node> node);
 
     // Maximum number of bytes could be stored in this cache.
     // i.e all (keys+values) must be less the _max_size
     std::size_t _max_size;
 
+    // Current number of bytes stored in this cache.
+    std::size_t _cur_size;
     // Main storage of lru_nodes, elements in this list ordered descending by "freshness": in the head
     // element that wasn't used for longest time.
     //
     // List owns all nodes
     std::unique_ptr<lru_node> _lru_head;
-
+    lru_node *_lru_tail;
     // Index of nodes from list above, allows fast random access to elements by lru_node#key
     std::map<std::reference_wrapper<std::string>, std::reference_wrapper<lru_node>, std::less<std::string>> _lru_index;
 };
