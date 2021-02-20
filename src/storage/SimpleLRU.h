687539d3eabd3517b14284eaa038b90df6b9c38c
Winking-maniac <gkgim@yadexru>
2021-02-20 17:58:24 +0300

    Prettify the code

--- src/storage/SimpleLRU.h
+++ src/storage/SimpleLRU.h
@@ -17,8 +17,8 @@ namespace Backend {
  */
 class SimpleLRU : public Afina::Storage {
 public:
-    SimpleLRU(size_t max_size = 1024) : _max_size(max_size), _cur_size(0), _lru_head(std::unique_ptr<lru_node>(nullptr)), _lru_tail(nullptr) {}
-
+    SimpleLRU(size_t max_size = 1024) : _max_size(max_size), _cur_size(0), \
+        _lru_head(std::unique_ptr<lru_node>(nullptr)), _lru_tail(nullptr) {}
     ~SimpleLRU() {
         _lru_index.clear();
 
